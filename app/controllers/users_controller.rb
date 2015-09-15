8aada7ae4f1e482aa0f879b2b11891fa011a27bd
Raxis <raxis86@gmail.com>
2015-09-15 17:13:18 +0800

    реализован механизм регистрации юзера

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Поздравляем с регистрацией на uromed-lkc.ru!"
       redirect_to @user
     else
       render 'new'
