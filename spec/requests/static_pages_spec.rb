7d4ef65a308625d83eefe6995422887f5e3d5fa7
Raxis <raxis86@gmail.com>
2015-09-15 10:12:45 +0800

    добавил аунтетификацию

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,33 +6,33 @@ describe "StaticPages" do
   
   	describe "Home page" do
     	it "should have the content 'Спасибо, что посетили наш сайт!'" do
-      		visit '/static_pages/home'
+      		visit root_path
       		expect(page).to have_content('Спасибо, что посетили наш сайт!')
     	end
 
     	it "should have the right title" do
-  			visit '/static_pages/home'
+  			visit root_path
   			expect(page).to have_title("#{base_title} | Главная")
 		end
 	end
 
 	describe "about page" do
 		it "should have the right title" do
-  			visit '/static_pages/about'
+  			visit about_path
   			expect(page).to have_title("#{base_title} | Деятельность")
 		end
 	end
 
 	describe "contacts page" do
 		it "should have the right title" do
-  			visit '/static_pages/contacts'
+  			visit contacts_path
   			expect(page).to have_title("#{base_title} | Контакты")
 		end
 	end
 
 	describe "other page" do
 		it "should have the right title" do
-  			visit '/static_pages/other'
+  			visit other_path
   			expect(page).to have_title("#{base_title} | Полезная информация")
 		end
   	end
