18b7d135b07b9a67b26dbce1d1f6df0d7a94e499
Raxis <raxis86@gmail.com>
2015-09-15 15:27:42 +0800

    успешная провальная регистрация юзера

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  #get 'users/new'
+  resources :users
   #get "static_pages/home"
   #get "static_pages/about"
   #get "static_pages/contacts"
