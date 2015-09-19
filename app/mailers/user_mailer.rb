5159b46057664540078c1d4958e0bcdb5d1ae148
Raxis <raxis86@gmail.com>
2015-09-19 15:05:31 +0800

    небольшие изменения - внес базовый урл в development среду

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
 	def welcome_email(user)
 		@user = user
-		@url = "www.uromed-lkc.ru/signin"#signin_url
+		@url = signin_url
 		mail(to: @user.email, subject: "Спасибо за регистрацию на сайте www.uromed-lkc.ru!")
 	end
 
