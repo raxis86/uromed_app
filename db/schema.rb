34ce1a22d8761ef3f98746556a9904f44a40580c
Raxis <your.raxis86@gmail.com>
2015-10-26 14:58:09 +0800

    перешел на postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150919071535) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "prices", force: :cascade do |t|
     t.string   "name"
     t.string   "cost"
@@ -33,8 +36,8 @@ ActiveRecord::Schema.define(version: 20150919071535) do
     t.string   "confirm_token"
   end
 
-  add_index "users", ["confirm_token"], name: "index_users_on_confirm_token"
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["confirm_token"], name: "index_users_on_confirm_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
